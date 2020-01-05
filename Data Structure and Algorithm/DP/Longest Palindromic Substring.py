f3b9cc7cfa3c280835ef19643644c291d627a40a
Yuanhui Zhang <yuanhuiz@usc.edu>
2020-01-04 18:46:36 -0800

    Create Longest Palindromic Substring.py

---
+++ Data Structure and Algorithm/DP/Longest Palindromic Substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        1. the center is the letter
+        2. the center is in-between the letter
+        """
+        palindrome = ''
+        
+        for i in range(len(s)):
+            
+            len1 = len(self.getlongestPalindrome(s,i,i))
+        
+            if len1 > len(palindrome):
+                palindrome = self.getlongestPalindrome(s,i,i)
+                
+            len2 = len(self.getlongestPalindrome(s,i,i+1))
+            
+            if len2 > len(palindrome):
+                palindrome = self.getlongestPalindrome(s,i,i+1)
+            
+        return palindrome
+    
+    def getlongestPalindrome(self,s,l,r):
+        while l >=0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
