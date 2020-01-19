506156624443bdd30d1871c80a0d00dbfb523f23
Yuanhui Zhang <yuanhuiz@usc.edu>
2020-01-18 17:01:19 -0800

    Update Merge Two Sorted Lists.py

--- Data Structure and Algorithm/Linked List/Merge Two Sorted Lists.py
+++ Data Structure and Algorithm/Linked List/Merge Two Sorted Lists.py
@@ -7,21 +7,53 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # iteration
-#         node = ListNode(0)
-#         dummy = node
+#         dummy = ListNode(0)
+#         cur = dummy
         
 #         while l1 and l2:
 #             if l1.val <= l2.val:
-#                 print('A')
-#                 node.next = l1
+#                 cur.next = l1
 #                 l1 = l1.next
-#                 print(dummy)
+#              
 #             else:
-#                 node.next = l2
+#                 cur.next = l2
 #                 l2 = l2.next
-#                 print(dummy)
+#             
 #             node = node.next
         
-#         node.next = l1 if l1 else l2
+#         cur.next = l1 if l1 else l2
 #         return dummy.next
-    
+---------------------------------------------------- Another Solution ----------------------------------------------------  
+#         if not l1: return l2
+#         if not l2: return l1
+#         if not l1 and not l2: return None
+#         list1 = []
+#         cur1 = l1 # create pointer1
+#         cur2 = l2 # create pointer2
+#         while cur1 and cur2:
+#             if cur1.val <= cur2.val:
+#                 list1.append(cur1.val)
+#                 cur1 = cur1.next
+#             else:
+#                 list1.append(cur2.val)
+#                 cur2 = cur2.next
+                
+#         # transform list to linked list
+
+#         head = ListNode(list1[0])
+#         print("this is {}".format(head))
+#         cur = head
+#         for x in list1[1:]:
+#             new_node = ListNode(x)
+#             cur.next = new_node
+#             cur = cur.next
+        
+#         if cur1:
+#             cur.next = cur1
+        
+#         if cur2:
+#             cur.next = cur2
+#         print(head)
+        
+#         return head
+          
